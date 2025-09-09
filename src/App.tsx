f573b92d9bed94caf349c07b5a853d6056fbdee2
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-09 20:45:25 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,44 @@
 // src/App.jsx
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as ort from "onnxruntime-web";
 
 function App() {
+  const [output, setOutput] = useState<any>(null);
   console.log({ ort });
   useEffect(() => {
     async function runInference() {
       // Path must be relative to public folder in Vite projects
-      const modelUrl = `mymodel.onnx`;
+      const modelUrl = `/mymodel.onnx`;
 
       // Create session
       const session = await ort.InferenceSession.create(modelUrl);
 
       // Example: prepare one input (float32 tensor)
-      const input = new ort.Tensor(
-        "float32",
-        [
-          /*data*/
-        ],
-        [
-          /*dimensions*/
-        ]
-      );
-      const feeds = { input_name: input };
+      const input = new ort.Tensor("float32", [0.5], [1, 1]);
+      console.log({ input });
+      const feeds = { x: input };
 
       // Run inference
       const results = await session.run(feeds);
-
+      setOutput(results);
       // Log output
-      console.log(results);
+      console.log({ results });
     }
     runInference();
   }, []);
 
-  return <div>ONNXRuntime Web Inference Example</div>;
+  return (
+    <div>
+      ONNXRuntime Web Inference Example
+      <div>
+        {/* Display output as JSON */}
+        <pre>
+          {/* {output ? JSON.stringify(output, null, 2) : "Running inference..."} */}
+          {output ? output?.linear_3?.cpuData["0"] : "Running...."}
+        </pre>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/onnxruntime-web/dist/*.wasm",
-          dest: ".",
-        },
-      ],
-    }),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "node_modules/onnxruntime-web/dist/*.wasm",
+    //       dest: ".",
+    //     },
+    //   ],
+    // }),
   ],
+  assetsInclude: ["**/*.onnx"],
+  optimizeDeps: {
+    exclude: ["onnxruntime-web"],
+  },
 });
